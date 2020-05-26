9d8c866c52a9fb19c121d6b09e8ae11244da0b97
Marcelo Lima <marcelosyd@gmail.com>
2020-05-25 23:48:13 -0300

    Desafio concluido

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,57 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Cliente invalido');
+    }
+
+    const productsIds = await this.productsRepository.findAllById(products);
+
+    if (productsIds.length === 0) {
+      throw new AppError('Não foram encontrados produtos');
+    }
+
+    const productsData = await this.productsRepository.findAllById(productsIds);
+
+    const orderProducts = productsData.map(productData => {
+      const orderProduct = products.find(
+        product => product.id === productData.id,
+      );
+
+      if (!orderProduct) {
+        throw new AppError('Produtos não encontrado.');
+      }
+
+      if (orderProduct.quantity > productData.quantity) {
+        throw new AppError('Quantidade insuficiente.');
+      }
+
+      return {
+        product_id: orderProduct.id,
+        price: productData.price,
+        quantity: orderProduct.quantity,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
